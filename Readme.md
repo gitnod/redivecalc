7a7c8eb5e10ec35a29e95e26aa4bb3dc55de2a7c
gitnod <>
2019-05-21 21:43:02 -0500

    initial commit

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# REDIVECALC
+
+A toy web app for simple calculations in Princess Connect Redive.
---
+++ indexScript.js
@@ -0,0 +1,12 @@
+var gachaCalcButton = document.getElementById("gachaCalcButton");
+var sarenCalcButton = document.getElementById("sarenCalcButton");
+
+// go to gacha calculator
+gachaCalcButton.addEventListener("click", function() {
+    window.location = './gacha/';
+})
+
+// go to saren distance calculator
+sarenCalcButton.addEventListener("click", function() {
+    window.location = './saren/';
+})
