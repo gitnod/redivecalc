7308d83955a27d35c7cd85de82ea67f2e5197720
gitnod <>
2019-05-25 13:06:13 -0500

    fix table encoding issue

--- data/characterPositionsTableGenerator.R
+++ data/characterPositionsTableGenerator.R
@@ -3,22 +3,24 @@ library(foreach)
 setwd("C:/GitRepo/redivecalc/data/")
 data.raw = read.csv("characterPositionsList.csv", header = FALSE, as.is = TRUE, na.strings = "")
 
-# rearrage the character list into data.frame
-data.df = foreach(i=1:nrow(data.raw), .combine = "rbind") %do% {
+# rearrage the character list into matrix
+data.mat = foreach(i=1:nrow(data.raw), .combine = "rbind") %do% {
   # read i-th row
   data.array = data.raw[i,]
   # read number of columns
   data.array.length = length(data.array) - sum(is.na(data.array))
   # read the representing name of the character
-  charTitle = data.array[data.array.length-1]
-  names(charTitle) = NULL
+  charTitle = as.character(data.array[data.array.length-1])
   # read all character names used for search
   charNames = paste(data.array[1:(data.array.length-1)], collapse = " ")
-  # read the character position as numeric
+  # read the character position
   charPosition = as.numeric(data.array[data.array.length])
   # return the rearranged row
-  data.frame(charTitle=charTitle, charNames=charNames, charPosition=charPosition)
+  matrix(c(charTitle, charNames, charPosition), nrow=1, ncol=3)
 }
 
-# write data.frame to csv
-write.csv(data.df, file = "characterPositionsTable.csv", row.names = FALSE)
+# record column names
+colnames(data.mat) = c("charTitle", "charNames", "charPosition")
+
+# write matrix to csv
+write.csv(data.mat, file = "characterPositionsTable.csv", row.names = FALSE)
