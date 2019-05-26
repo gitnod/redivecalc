e705efef24ebd619c3a05140e8b7310ce303d1de
gitnod <>
2019-05-26 14:23:50 -0500

    add limited character status

--- data/characterPositionsTableGenerator.R
+++ data/characterPositionsTableGenerator.R
@@ -2,6 +2,7 @@
 library(foreach)
 setwd("C:/GitRepo/redivecalc/data/")
 data.raw = read.csv("characterPositionsList.csv", header = FALSE, as.is = TRUE, na.strings = "")
+data.limited = read.csv("characterLimitedList.csv", header = FALSE, as.is = TRUE, na.strings = "")$V1
 
 # rearrage the character list into matrix
 data.mat = foreach(i=1:nrow(data.raw), .combine = "rbind") %do% {
@@ -15,12 +16,14 @@ data.mat = foreach(i=1:nrow(data.raw), .combine = "rbind") %do% {
   charNames = paste(data.array[1:(data.array.length-1)], collapse = " ")
   # read the character position
   charPosition = as.numeric(data.array[data.array.length])
+  # check if the character is limited
+  charLimited = as.numeric(sum(foreach(j=1:length(data.limited), .combine = "c") %do% { regexpr(data.limited[j], charNames) } != (-1)) > 0)
   # return the rearranged row
-  matrix(c(charTitle, charNames, charPosition), nrow=1, ncol=3)
+  matrix(c(charTitle, charNames, charPosition, charLimited), nrow=1, ncol=4)
 }
 
 # record column names
-colnames(data.mat) = c("charTitle", "charNames", "charPosition")
+colnames(data.mat) = c("charTitle", "charNames", "charPosition", "charLimited")
 
 # write matrix to csv
 write.csv(data.mat, file = "characterPositionsTable.csv", row.names = FALSE, fileEncoding = "UTF-8")
